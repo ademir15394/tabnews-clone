2334e6f7ce1db900d1bda944cac7f8c2df03f5c5
ademir15394 <ademirzinhoperes@gmail.com>
2026-03-11 16:33:42 -0300

    feat: add pages

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/SU/hlETospopXNKH4HVuK9ZDTDiC0geESHpyxXoFkw=\"\n}"
---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // O Next.js 16 já configura tudo automaticamente aqui
+};
+
+module.exports = nextConfig;
